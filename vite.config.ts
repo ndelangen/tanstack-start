2df34873776005c61075ef3fb08a903e7a1aae59
Norbert de Langen <ndelangen@me.com>
2026-03-06 22:26:47 +0100

    init

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+
+const config = defineConfig({
+  build: {
+    assetsDir: 'public', // will make your static assets appear under /public/
+  },
+  publicDir: 'public',
+  plugins: [
+    devtools(),
+    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    tanstackStart({
+      
+      srcDirectory: './src/app',
+      spa: {
+        enabled: true,
+      } 
+    }),
+    viteReact(),
+  ],
+})
+
+export default config
